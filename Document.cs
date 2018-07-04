ec99ea7455ba94ae7353eb175194eef42c43dfab
jpb8vf <34172596+jpb8vf@users.noreply.github.com>
2018-07-03 19:33:26 -0500

    Add files via upload

---
+++ Document.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+using System.IO;
+
+namespace DocumentApplication
+{
+
+    class Document
+    {
+
+        static void Main(string[] args)
+        {
+
+            try
+            {
+
+                Console.WriteLine("Document");
+
+                Console.WriteLine("document title:");
+
+                string name = Console.ReadLine();
+
+                Console.WriteLine("document content:");
+
+                string content = Console.ReadLine();
+
+                string filename = name + ".txt";
+
+                string path = Environment.CurrentDirectory + "/" + filename;
+
+                if (!File.Exists(path))
+
+                {
+
+                    File.WriteAllText(path, content);
+
+                }
+
+                Console.WriteLine(filename + " saved successfully. The document has " + content.Length + " characters");
+
+            }
+
+            catch (Exception e)
+
+            {
+
+                Console.WriteLine(e);
+
+            }
+
+        }
+
+    }
+
+}
+
---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+using System.IO;
+
+namespace DocumentApplication
+{
+
+    class Document
+    {
+
+        static void Main(string[] args)
+        {
+
+            try
+            {
+
+                Console.WriteLine("Document");
+
+                Console.WriteLine("Enter Document Title:");
+
+                string name = Console.ReadLine();
+
+                Console.WriteLine("Enter Document Content:");
+
+                string content = Console.ReadLine();
+
+                string filename = name + ".txt";
+
+                string path = Environment.CurrentDirectory + "/" + filename;
+
+                if (!File.Exists(path))
+
+                {
+
+                    File.WriteAllText(path, content);
+
+                }
+
+                Console.WriteLine(filename + " was successfully saved. The document contains " + content.Length + " characters");
+
+            }
+
+            catch (Exception e)
+
+            {
+
+                Console.WriteLine(e);
+
+            }
+
+        }
+
+    }
+
+}
+
